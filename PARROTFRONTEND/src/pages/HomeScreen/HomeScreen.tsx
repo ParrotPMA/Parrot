91b74c86dec66478aeb06ec72df601c0f5d3e572
praveen-suggaa <praveen@suggaa.in>
2024-06-01 21:42:55 +0530

    testing commit part 2

--- PARROTFRONTEND/src/pages/HomeScreen/HomeScreen.tsx
+++ PARROTFRONTEND/src/pages/HomeScreen/HomeScreen.tsx
@@ -2,7 +2,7 @@ import React from "react";
 interface HomeScreenProps {
   id: string;
 }
-// checking github commit
+// checking github commit part 2
 const HomeScreen: React.FC<HomeScreenProps> = ({ id }) => {
   return (
     <div className="flex flex-row justify-center items-center bg-blue">
