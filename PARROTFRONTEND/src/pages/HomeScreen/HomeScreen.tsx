aeee395fb59d347cc64584b86566b067f326244a
Abhi-jha12 <jhaabhishek887@gmail.com>
2024-06-02 16:25:13 +0530

    wip homescreen added text

--- PARROTFRONTEND/src/pages/HomeScreen/HomeScreen.tsx
+++ PARROTFRONTEND/src/pages/HomeScreen/HomeScreen.tsx
@@ -7,7 +7,7 @@ const HomeScreen: React.FC<HomeScreenProps> = ({ id }) => {
   return (
       <div className="flex flex-row justify-center items-center bg-blue md:bg-red-500 lg:bg-amber-500">
           <text className="text-lg text-red-700 ">{id}</text>
-          <text className="text-lg text-red-700 ">{id}</text>
+          <text className="text-lg text-red-700 ">abhi</text>
       </div>
   );
 };
