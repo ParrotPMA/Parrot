83a5d2ca0eb33525088edee48ed90b4915295fa6
Pravenn18 <insanepraveen2018@gamil.com>
2024-06-03 22:47:15 +0530

    Added search-bar.tsx

---
+++ PARROTFRONTEND/src/components/search-bar.tsx
@@ -0,0 +1,31 @@
+import React, { useState, ChangeEvent } from 'react';
+
+interface SearchBarProps {
+  onSearch: (query: string) => void;
+  className?: string
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
+  const [query, setQuery] = useState('');
+
+//   TODO: update when backend api is ready
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const newQuery = event.target.value;
+    setQuery(newQuery);
+    onSearch(newQuery);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={query}
+        onChange={handleInputChange}
+        placeholder="Search"
+        className="pl-4 border border-[#D9D9D9] rounded-3xl h-9 focus:outline-none focus:ring-gray-500"
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
