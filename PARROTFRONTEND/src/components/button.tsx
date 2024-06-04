fbb98f8a8fe35049dd67f9c49c5a4281a0bbac09
Pravenn18 <insanepraveen2018@gamil.com>
2024-06-04 23:11:55 +0530

    Modified button.tsx

--- PARROTFRONTEND/src/components/button.tsx
+++ PARROTFRONTEND/src/components/button.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 
 interface ButtonProps {
-  className: string;
-  title: string;
-  image?: string;
+  className?: string;
+  title?: string;
+  image1?: string;
+  image2?: string;
+  isIconButton?: boolean;
 }
 
-export const Button: React.FC<ButtonProps> = ({ className, title, image }) => {
+export const Button: React.FC<ButtonProps> = ({ className, title, image1, image2, isIconButton }) => {
   return (
-    <div className={`bg-[#00C2E0] h-9 flex items-center justify-center rounded-md ${className}`}>
-        {image && <img src={image} alt="" className='h-6 w-6 flex' />}
-        <button className="text-white">{title}</button>
+    <div className={`${isIconButton ? 'bg-white' : `bg-[#388BFF] h-12 flex items-center justify-center rounded-sm ${className}`}`}>
+      {isIconButton && <img src={image1} className='h-5 w-5 hover:cursor-pointer'/>}
+        {image2 && !isIconButton && <img src={image2} alt="" className='h-6 w-6 flex'/>}
+        {!isIconButton && <button className="text-white">{title}</button>}
     </div>
   );
 };
