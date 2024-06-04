7175a974187156cdbf49ce48dc0091af026ad95e
Pravenn18 <insanepraveen2018@gamil.com>
2024-06-05 02:22:02 +0530

    modified your-workspace.tsx

--- PARROTFRONTEND/src/components/your-workspace.tsx
+++ PARROTFRONTEND/src/components/your-workspace.tsx
@@ -1,8 +1,10 @@
+import { WorkspaceTopBar } from "./workspace-topbar"
 
 export const YourWorkspace = () => {
   return (
     <div>
         <h3 className="text-base font-custom font-bold">YOUR WORKSPACE</h3>
+        <WorkspaceTopBar title="Praveen Kumar's Workspace"/>
     </div>
   )
 }
