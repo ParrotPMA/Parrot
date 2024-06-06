e26de6df76e91bf751f9e83a02f3e042e50ec9ff
raymondreddd <raymondreddington40@gmail.com>
2024-06-07 00:31:17 +0530

    Feat: Separate components

--- PARROTFRONTEND/src/components/InteractiveCard.tsx
+++ PARROTFRONTEND/src/components/InteractiveCard.tsx
@@ -1,16 +1,36 @@
-import { motion, useDragControls } from "framer-motion";
-const InteractiveCard = () => {
-  const controls = useDragControls();
-
-  function startDrag(event) {
-    controls.start(event);
-  }
-
+import { motion } from "framer-motion";
+import DropIndicator from "./DropIndicator";
+/**
+ *
+ * @Components
+ * 1. DropIndicator a violet line to tell on which column we are dropping/dragging the card
+ * @param title
+ * @returns
+ */
+const InteractiveCard = ({
+  title,
+  id,
+  column,
+  handleDragStart,
+}: {
+  title: string;
+  id: string;
+  column: number;
+  handleDragStart: any;
+  startTime: Date;
+  authors: string[];
+}) => {
   return (
     <>
-      <div onPointerDown={startDrag} />
-      <motion.div className="bg-red w-5 h-6" drag="x" dragControls={controls}>
-        WOW{" "}
+      <DropIndicator beforeId={id} column={column} />
+      <motion.div
+        layout
+        layoutId={id}
+        draggable="true"
+        onDragStart={(e) => handleDragStart(e, { title, id, column })}
+        className="cursor-grab rounded border border-neutral-700 bg-neutral-800 p-3 active:cursor-grabbing"
+      >
+        <p className="text-sm text-neutral-100">{title}</p>
       </motion.div>
     </>
   );
