b6f0aafcaeb21bedb9f46db5e2df2793754cb444
Pravenn18 <insanepraveen2018@gamil.com>
2024-06-16 00:45:17 +0530

    updated workspace-tiles

--- PARROTFRONTEND/src/components/workspace-tiles.tsx
+++ PARROTFRONTEND/src/components/workspace-tiles.tsx
@@ -1,15 +1,18 @@
+import { Link } from 'react-router-dom';
 import kanbanImage from '../assets/images/kanban.jpg';
 
 export const WorkspaceTiles = () => {
   return (
     <div className="relative w-48 h-24">
-      <div
-        className="relative flex justify-between w-full h-full bg-cover bg-center p-2 mt-4 rounded-sm shadow-lg transition-all duration-300 group hover:cursor-pointer"
-        style={{ backgroundImage: `url(${kanbanImage})` }}
-      >
-        <p className="text-base font-custom font-semibold text-white z-10">Kanban Template</p>
-        <div className="absolute inset-0 bg-black opacity-10 group-hover:opacity-25 transition-opacity duration-200 rounded-sm"></div>
-      </div>
+      <Link to={'/kanban'}>
+        <div
+          className="relative flex justify-between w-full h-full bg-cover bg-center p-2 mt-4 rounded-sm shadow-lg transition-all duration-300 group hover:cursor-pointer"
+          style={{ backgroundImage: `url(${kanbanImage})` }}
+        >
+          <p className="text-base font-custom font-semibold text-white z-10">Kanban Template</p>
+          <div className="absolute inset-0 bg-black opacity-10 group-hover:opacity-25 transition-opacity duration-200 rounded-sm"></div>
+        </div>
+      </Link>
     </div>
   );
 };
