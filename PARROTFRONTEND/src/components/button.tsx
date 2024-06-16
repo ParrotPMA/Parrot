62b96e162b29321f29b7ab99424b555e2af0bc9a
Pravenn18 <insanepraveen2018@gamil.com>
2024-06-17 00:53:04 +0530

    Updated button.tsx

--- PARROTFRONTEND/src/components/button.tsx
+++ PARROTFRONTEND/src/components/button.tsx
@@ -6,20 +6,26 @@ type ButtonProps = {
   image1?: string;
   image2?: string;
   isIconButton?: boolean;
-  onStateChange?: (newState: boolean) => void
+  onStateChange?: (newState: boolean) => void;
+  enabled?: boolean;
 }
-export const Button: React.FC<ButtonProps> = ({ className, title, image1, image2, isIconButton, onStateChange }) => {
+
+export const Button: React.FC<ButtonProps> = ({ className, title, image1, image2, isIconButton, onStateChange, enabled = true }) => {
   const [isActive, setIsActive] = useState(false);
 
   const handleClick = () => {
-    setIsActive(!isActive);
-    onStateChange && onStateChange(isActive);
+    enabled && setIsActive(!isActive);
+    enabled && onStateChange && onStateChange(isActive);
   };
+
   return (
-    <div className={`${isIconButton ? 'bg-white' : `bg-[#388BFF] h-12 flex items-center justify-center rounded-sm ${className}`}`} onClick={handleClick} >
+    <div 
+      className={`${isIconButton ? 'bg-white' : enabled ? 'bg-[#388BFF]' : 'bg-gray-200'} h-12 flex items-center justify-center rounded-sm ${className}`} 
+      onClick={handleClick}
+    >
       {isIconButton && <img src={image1} className='h-5 w-5 hover:cursor-pointer'/>}
-        {image2 && !isIconButton && <img src={image2} alt="" className='h-4 w-4 flex'/>}
-        {!isIconButton && <button className="text-white pl-2 font-medium">{title}</button>}
+      {image2 && !isIconButton && <img src={image2} alt="" className='h-4 w-4 flex'/>}
+      {!isIconButton && <button className="text-white pl-2 font-medium">{title}</button>}
     </div>
   );
 };
