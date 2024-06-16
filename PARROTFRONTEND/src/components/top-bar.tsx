395c5d3bbe44fdc76cf54c3493be5e3665814d5d
Pravenn18 <insanepraveen2018@gamil.com>
2024-06-16 17:01:07 +0530

    updated top-bar

--- PARROTFRONTEND/src/components/top-bar.tsx
+++ PARROTFRONTEND/src/components/top-bar.tsx
@@ -9,12 +9,17 @@ import bell from '../assets/images/bell.png';
 import info from '../assets/images/info.png';
 import user from '../assets/images/user.png';
 import NotificationModal from './notification-modal';
+import { CreateBoardModal } from './create-board-modal';
+import CreateBoard from './create-board';
 
 const dropdownLists: string[] = ["Workspace", "Recent", "Starred", "Templates"];
 
 export const TopBar = () => {
   const [, setSearchResults] = useState<string[]>([]);
   const [isNotification, setIsNotification] = useState(false);
+  const [isCreateBoard, setIsCreateBoard] = useState(false);
+  const [isCreateBoardModal, setIsCreateBoardModal] = useState(false);
+
 
   // TODO: Update when api is available or backend is ready
   const handleSearch = (query: string): void => {
@@ -28,6 +33,14 @@ export const TopBar = () => {
   const handleNotification = (newState: boolean) => {
     setIsNotification(!newState);
   }
+  const handleCreateBoard = (newState: boolean) => {
+    setIsCreateBoard(!newState);
+  }
+  const handleOnStateChange = (data: boolean) => {
+    setIsCreateBoardModal(!data);
+    setIsCreateBoard(!isCreateBoard);
+  }
+
   return (
     <div className='flex flex-col md:flex-row justify-between items-center w-full px-7 pt-6 bg-[#F5F5F5] h-12 pb-5'>
       <div className='flex items-center mb-4 md:mb-0'>
@@ -36,7 +49,7 @@ export const TopBar = () => {
         {dropdownLists.map((item: string, index: number) => (
           <DropDown title={item} key={index}/>
         ))}
-        <Button className='w-20 font-normal h-8 ml-4' title='Create'/>
+        <Button className='w-20 font-normal h-8 ml-4' title='Create' onStateChange={handleCreateBoard}/>
       </div>
       <div className='flex items-center space-x-4'>
         <SearchBar onSearch={handleSearch}/>
@@ -45,6 +58,8 @@ export const TopBar = () => {
         <Button isIconButton image1={user} className='px-1'/>
       </div>
       {isNotification && <NotificationModal/>}
+      {isCreateBoard && <CreateBoardModal onStateChange={handleOnStateChange}/>}
+      {isCreateBoardModal && <CreateBoard/>}
     </div>
   )
 }
