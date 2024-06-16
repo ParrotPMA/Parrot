b8e15c2ca0b8e9f418ea130f789e8099a52d4aa8
Pravenn18 <insanepraveen2018@gamil.com>
2024-06-16 17:04:01 +0530

    updated workspace-tiles

--- PARROTFRONTEND/src/components/workspace-tiles.tsx
+++ PARROTFRONTEND/src/components/workspace-tiles.tsx
@@ -1,15 +1,27 @@
 import { Link } from 'react-router-dom';
 import kanbanImage from '../assets/images/kanban.jpg';
 
-export const WorkspaceTiles = () => {
+interface WorkspaceTilesProps {
+  height?: string;
+  width?: string;
+  isCustom?: boolean;
+  boardImage?: string;
+}
+let image = kanbanImage;
+export const WorkspaceTiles: React.FC<WorkspaceTilesProps> = ({ 
+  width=48,
+  height=24,
+  isCustom,
+  boardImage=image,
+ }) => {
   return (
-    <div className="relative w-48 h-24">
+    <div className={`relative w-${width} h-${height}`}>
       <Link to={'/kanban'}>
         <div
           className="relative flex justify-between w-full h-full bg-cover bg-center p-2 mt-4 rounded-sm shadow-lg transition-all duration-300 group hover:cursor-pointer"
-          style={{ backgroundImage: `url(${kanbanImage})` }}
+          style={{ backgroundImage: `url(${boardImage})` }}
         >
-          <p className="text-base font-custom font-semibold text-white z-10">Kanban Template</p>
+          {!isCustom && <p className="text-base font-custom font-semibold text-white z-10">Kanban Template</p>}
           <div className="absolute inset-0 bg-black opacity-10 group-hover:opacity-25 transition-opacity duration-200 rounded-sm"></div>
         </div>
       </Link>
